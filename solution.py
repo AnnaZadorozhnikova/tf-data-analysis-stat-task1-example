9c47c84ad1bb4a9e8542891087bb4200648202d6
AnnaZadorozhnikova <58693716+AnnaZadorozhnikova@users.noreply.github.com>
2023-04-06 01:14:15 +0300

    Update solution.py

--- solution.py
+++ solution.py
@@ -1,5 +1,6 @@
 import pandas as pd
 import numpy as np
+import math
 
 
 chat_id = 393669278 # Ваш chat ID, не меняйте название переменной
@@ -8,4 +9,5 @@ def solution(x: np.array) -> float:
     # Измените код этой функции
     # Это будет вашим решением
     # Не меняйте название функции и её аргументы
-    return np.log(x-635).mean() # Ваш ответ
+    x = np.array([math.log(i) for i in x-635]) 
+    return x.mean() # Ваш ответ
